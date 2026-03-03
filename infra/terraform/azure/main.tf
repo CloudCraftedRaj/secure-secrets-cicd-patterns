253d9771782bc5913f70d4765016d1cc7f96a326
CloudCraftedRaj <225103695+CloudCraftedRaj@users.noreply.github.com>
2026-03-02 23:50:22 -0500

    update main.tf to enable rbac and update workflow to fix "line 1:Length: ${#***}: bad substitution"

--- infra/terraform/azure/main.tf
+++ infra/terraform/azure/main.tf
@@ -13,6 +13,7 @@ resource "azurerm_key_vault" "kv" {
   sku_name                   = "standard"
   purge_protection_enabled   = false
   soft_delete_retention_days = 7
+  enable_rbac_authorization  = true
 }
 
 # Example secret (placeholder) — we’ll update later
@@ -20,6 +21,9 @@ resource "azurerm_key_vault_secret" "demo_secret" {
   name         = "demo-api-key"
   value        = "replace-me-later"
   key_vault_id = azurerm_key_vault.kv.id
+  depends_on = [
+    azurerm_role_assignment.current_user_kv_secrets_officer
+  ]
 }
 
 # Azure AD Application for GitHub OIDC federation
@@ -34,6 +38,7 @@ resource "azurerm_role_assignment" "kv_secrets_user" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_user_assigned_identity.gha.principal_id
+  depends_on = [azurerm_key_vault.kv]
 }
 
 # Federated credential so GitHub Actions can login without secrets
@@ -44,4 +49,10 @@ resource "azurerm_federated_identity_credential" "github" {
   issuer              = "https://token.actions.githubusercontent.com"
   parent_id           = azurerm_user_assigned_identity.gha.id
   subject             = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}"
+}
+
+resource "azurerm_role_assignment" "current_user_kv_secrets_officer" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
